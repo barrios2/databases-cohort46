3c5fc235d52d51770b668fa139dc18b28a987716
Fressia <fressiabarrios2@gmail.com>
2024-02-28 18:31:57 +0100

    applied changes to ex1 according to feedback

--- Week1/assignment/meetup-database.js
+++ Week1/assignment/meetup-database.js
@@ -5,7 +5,6 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
-  database: 'meetup',
 });
 
 const execQuery = util.promisify(connection.query.bind(connection));
@@ -14,45 +13,50 @@ async function coreDatabase() {
   const CREATE_INVITEE_TABLE = `
     CREATE TABLE IF NOT EXISTS invitee (
       invitee_no INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-      invitee_name VARCHAR(100),
-      invited_by VARCHAR(100) NOT NULL
+      invitee_name VARCHAR(100)
   );`
+  const ALTER_INVITEE_TABLE = `
+    ALTER TABLE invitee
+    ADD COLUMN invited_by INT,
+    ADD CONSTRAINT fk_invited_by FOREIGN KEY (invited_by)
+    REFERENCES invitee(invitee_no)
+  `
   const CREATE_ROOM_TABLE = `
     CREATE TABLE IF NOT EXISTS room (
       room_no INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
       room_name VARCHAR(100),
-      floor_number INT NOT NULL
+      floor_number TINYINT NOT NULL 
   );`
   const CREATE_MEETING_TABLE = `
     CREATE TABLE IF NOT EXISTS meeting (
       meeting_no INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
       meeting_title TEXT NOT NULL,
-      starting_time TIME,
-      ending_time TIME,
+      starting_time DATETIME NOT NULL,
+      ending_time DATETIME NOT NULL,
       room_no INT NOT NULL,
-      FOREIGN KEY (room_no) REFERENCES Room(room_no)
+      FOREIGN KEY (room_no) REFERENCES room(room_no)
   );`
 
   const invitee = [
     {
       invitee_name: 'Fressia', 
-      invited_by: 'Yos',
+      invited_by: 2,
     },
     {
       invitee_name: 'Martin', 
-      invited_by: 'Carlos',
+      invited_by: 4,
     },
     {
       invitee_name: 'Gaby', 
-      invited_by: 'Emma',
+      invited_by: 1,
     },
     {
       invitee_name: 'Mildred', 
-      invited_by: 'Antonio',
+      invited_by: 3,
     },
     {
       invitee_name: 'Michelle', 
-      invited_by: 'Jose',
+      invited_by: 5,
     }
   ];
 
@@ -77,37 +81,37 @@ async function coreDatabase() {
       room_name: 'Ivory',
       floor_number: 2,
     },
-  ]
+  ];
 
   const meeting = [
     {
       meeting_title: 'Budget Planning',
-      starting_time: '10:00:00', 
-      ending_time: '11:00:00', 
+      starting_time: '2024-01-23 10:00:00', 
+      ending_time: '2024-01-23 11:00:00', 
       room_no: 3,
     },
     {
       meeting_title: 'Catch up', 
-      starting_time: '9:00:00', 
-      ending_time: '10:00:00', 
+      starting_time: '2024-01-22 9:00:00', 
+      ending_time: '2024-01-22 10:00:00', 
       room_no: 2,
     },
     {
       meeting_title: 'Monthly stand-up', 
-      starting_time: '08:30:00', 
-      ending_time: '09:00:00',
+      starting_time: '2024-01-26 08:30:00', 
+      ending_time: '2024-01-26 09:00:00',
       room_no: 1,
     },
     {
       meeting_title: 'Demo',
-      starting_time: '11:30:00', 
-      ending_time: '12:00:00', 
+      starting_time: '2024-02-20 11:30:00', 
+      ending_time: '2024-02-20 12:00:00', 
       room_no: 2,
     },
     {
       meeting_title: 'Onboarding', 
-      starting_time: '14:00:00', 
-      ending_time: '15:30:00',
+      starting_time: '2024-02-22 14:00:00', 
+      ending_time: '2024-02-22 15:30:00',
       room_no: 3,
     },
   ];
@@ -115,20 +119,41 @@ async function coreDatabase() {
   connection.connect();
 
   try {
-    await Promise.all([
-      execQuery(CREATE_INVITEE_TABLE), 
-      execQuery(CREATE_ROOM_TABLE), 
-      execQuery(CREATE_MEETING_TABLE)
-    ]);
-    
-    await Promise.all(invitee.map(inv => execQuery('INSERT INTO invitee SET ?', inv)));
-    await Promise.all(room.map(rm => execQuery('INSERT INTO room SET ?', rm)));
-    await Promise.all(meeting.map(mt => execQuery('INSERT INTO meeting SET ?', mt)));
+    execQuery('DROP DATABASE IF EXISTS meetup');
+    execQuery('CREATE DATABASE IF NOT EXISTS meetup');
+    execQuery('USE meetup');
+    execQuery(CREATE_INVITEE_TABLE);
+    execQuery(ALTER_INVITEE_TABLE);
+    execQuery(CREATE_ROOM_TABLE);
+    execQuery(CREATE_MEETING_TABLE);
+
+    // temporarily disabling checking for foreign keys in the database - https://www.sqlines.com/mysql/set_foreign_key_checks
+    execQuery('SET FOREIGN_KEY_CHECKS=0');
+
+    await Promise.all(
+      invitee.map(inv => {
+        execQuery('INSERT INTO invitee SET ?', inv)
+      })
+    );
+
+    // enabling foreign key constraints check
+    execQuery('SET FOREIGN_KEY_CHECKS=1');
+
+    await Promise.all(room.map(rm => {
+      execQuery('INSERT INTO room SET ?', rm)
+      })
+    );
+
+    await Promise.all(meeting.map(mt => {
+      execQuery('INSERT INTO meeting SET ?', mt)
+      })
+    );
+
   } catch (error) {
     console.error(error);
-  }
+  };
 
   connection.end();
-}
+};
 
 coreDatabase();
