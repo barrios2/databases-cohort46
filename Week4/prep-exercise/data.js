98d81945a188e3c3bbd193b4ea79f5dda4f0d450
Fressia <fressiabarrios2@gmail.com>
2024-03-09 15:09:38 +0100

    created data.js file containing recipes data

---
+++ Week4/prep-exercise/data.js
@@ -0,0 +1,73 @@
+const data = [
+  {
+    recipe_name: 'No-Bake Cheesecake',
+    categories: ['No-bake', 'Cake', 'Vegetarian'],
+    ingredients: [
+      'Condensed milk', 'Cream Cheese', 'Lemon Juice', 'Pie Crust', 'Cherry Jam'
+    ],
+    steps: [
+      'Beat Cream Cheese',
+      'Add condensed Milk and blend',
+      'Add Lemon Juice and blend',
+      'Add the mix to the pie crust',
+      'Spread the Cherry Jam',
+      'Place in refrigerator for 3h' 
+    ]
+  },
+  {
+    recipe_name: 'Roasted Brussels Sprouts',
+    categories: ['Vegan', 'Gluten-free'],
+    ingredients: [
+      'Brussels Sprouts', 'Lemon juice', 'Sesame seeds', 'Pepper', 'Salt', 'Olive oil'
+    ],
+    steps: [
+      'Preheat the oven',
+      'Mix the ingredients in a bowl',
+      'Spread the mix on baking sheet',
+      'Bake for 30'
+    ]
+  },
+  {
+    recipe_name: 'Mac & Cheese',
+    categories: 'Vegetarian',
+    ingredients: [
+      'Macaroni',
+      'Butter',
+      'Flour',
+      'Salt',
+      'Pepper',
+      'Milk',
+      'Shredded Cheddar cheese'
+    ],
+    steps: [
+      'Cook Macaroni for 8',
+      'Melt butter in a saucepan',
+      'Add flour, salt, pepper and mix',
+      'Add Milk and mix',
+      'Cook until mix is smooth',
+      'Add cheddar cheese',
+      'Add the macaroni'
+    ]
+  },
+  {
+    recipe_name: 'Tamagoyaki Japanese Omelette',
+    categories: ['Japanese', 'Vegetarian'],
+    ingredients: [
+      'Eggs',
+      'Soy sauce',
+      'Sugar',
+      'Salt',
+      'Olive Oil'
+    ],
+    steps: [
+      'Beat the eggs',
+      'Add soya sauce, sugar and salt',
+      'Add oil to a sauce pan', 
+      'Bring to medium heat',
+      'Add some mix to the sauce pan',
+      'Let is cook for 1'
+    ]
+  },
+]
+
+module.exports = data;
